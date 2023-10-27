7e4440af17ce88545eecf0d2227e00c46e58bf55
Iriogbe Peter <iriogbepeter22@gmail.com>
2023-10-27 15:17:25 +0100

    goodle multilingual translator added to the site

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -21,6 +21,21 @@
     @include('web.layouts.bottom.footer')
     @include('web.layouts.bottom.scripts')
 
+    <!-- Google Translate -->
+
+    <script type="text/javascript">
+        function googleTranslateElementInit() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'en'
+            }, 'google_translate_element');
+        }
+    </script>
+
+     <!-- End Google Translate -->
+
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
+    </script>
+
     <!-- Mirrored from thegenius.co/html/loanplus/preview/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 13 Oct 2023 22:06:38 GMT -->
 </body>
 
